8a533d2c5809a086aca22b3f4948d3de6d0daf5c
brusoto <143963365+brusoto@users.noreply.github.com>
2025-05-18 13:33:42 -0300

    atulizando urls

--- estetica_na_palma_da_mao/urls.py
+++ estetica_na_palma_da_mao/urls.py
@@ -1,7 +1,14 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.index, name="principal"),
+    path("agendamento", views.agendamento, name="agendamento"),
+    path("cadastro", views.cadastro, name="cadastro"),
+    path("procedimento", views.procedimento, name="procedimento"),
+    path("fornecedor/cadastro/", views.cadastro_fornecedor, name="cadastro_fornecedor"),
     path('login/', views.login_view, name='login'),
     path('menu/', views.menu_view, name='menu'),
 ]
