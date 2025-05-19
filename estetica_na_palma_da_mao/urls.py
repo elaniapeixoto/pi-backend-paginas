dd1b62e501bb646dd6bbec92b5cd3add0b4878ae
Rafael Souza dos Santos <64049095+Radoido@users.noreply.github.com>
2025-05-18 21:23:00 -0300

    arrumando url

--- estetica_na_palma_da_mao/urls.py
+++ estetica_na_palma_da_mao/urls.py
@@ -1,3 +1,4 @@
+
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -6,9 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="principal"),
     path("agendamento", views.agendamento, name="agendamento"),
-    path("cadastro_pessoa", views.cadastro_pessoa, name="cadastro_pessoa"),
+    path("cadastro", views.cadastro, name="cadastro"),
     path("procedimento", views.procedimento, name="procedimento"),
     path("fornecedor/cadastro/", views.cadastro_fornecedor, name="cadastro_fornecedor"),
-    path('login/', views.login, name='login'),
-    path('menu/', views.menu, name='menu'),
 ]
