5c23c37b0de22db7ba325bdc5c0cb706fc74ab61
Rafael Souza dos Santos <64049095+Radoido@users.noreply.github.com>
2025-05-18 22:06:34 -0300

    ajustando erros

--- estetica_na_palma_da_mao/urls.py
+++ estetica_na_palma_da_mao/urls.py
@@ -3,12 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", views.index, name="principal"),
-    path("agendamento", views.agendamento, name="agendamento"),
+    path('', views.index, name='index'),
+    path('menu', views.menu, name='menu'),
+    path("cadastro_agendamento", views.agendamento, name="agendamento"),
     path("cadastro_pessoa", views.cadastro_pessoa, name="cadastro_pessoa"),
-    path("procedimento", views.procedimento, name="procedimento"),
-    path("fornecedor/cadastro/", views.cadastro_fornecedor, name="cadastro_fornecedor"),
+    path("cadastro_procedimento", views.procedimento, name="procedimento"),
+    path("cadastro_fornecedor/", views.cadastro_fornecedor, name="cadastro_fornecedor"),
     path('login/', views.login, name='login'),
-    path('menu/', views.menu, name='menu'),
 ]
