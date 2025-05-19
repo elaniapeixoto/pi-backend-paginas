e064f1adeaf41a270ceb9729b4b5f6dd5eebb63f
Rafael Souza dos Santos <64049095+Radoido@users.noreply.github.com>
2025-05-18 21:17:45 -0300

    subindo

--- estetica_na_palma_da_mao/urls.py
+++ estetica_na_palma_da_mao/urls.py
@@ -1,4 +1,21 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.menu, name='menu'),
+    path("agendamento", views.agendamento, name="agendamento"),
+    path("cadastro_pessoa", views.cadastro_pessoa, name="cadastro_pessoa"),
+    path("procedimento", views.procedimento, name="procedimento"),
+    path("fornecedor/cadastro/", views.cadastro_fornecedor, name="cadastro_fornecedor"),
+    path('login/', views.login, name='login'),
+]
+=======
+>>>>>>> Stashed changes
 =======
 <<<<<<< HEAD
 from django.contrib import admin
@@ -28,6 +45,10 @@ urlpatterns = [
     path("fornecedor/cadastro/", views.cadastro_fornecedor, name="cadastro_fornecedor"),
 ]
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+>>>>>>> 54b7b7424b2c9e3e572bdf60713a6c45b6bcda5a
+>>>>>>> Stashed changes
 =======
 >>>>>>> 54b7b7424b2c9e3e572bdf60713a6c45b6bcda5a
 >>>>>>> Stashed changes
