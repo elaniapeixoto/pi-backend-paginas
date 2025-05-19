d3c506d68b4d58eb1196932b6e0f640b44438e34
Rafael Souza dos Santos <64049095+Radoido@users.noreply.github.com>
2025-05-18 22:15:50 -0300

    correção para login

--- estetica_na_palma_da_mao/urls.py
+++ estetica_na_palma_da_mao/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('menu', views.menu, name='menu'),
-    path("cadastro_agendamento", views.agendamento, name="agendamento"),
-    path("cadastro_pessoa", views.cadastro_pessoa, name="cadastro_pessoa"),
-    path("cadastro_procedimento", views.procedimento, name="procedimento"),
+    path('menu/', views.menu, name='menu'),
+    path("cadastro_agendamento/", views.agendamento, name="agendamento"),
+    path("cadastro_pessoa/", views.cadastro_pessoa, name="cadastro_pessoa"),
+    path("cadastro_procedimento/", views.procedimento, name="procedimento"),
     path("cadastro_fornecedor/", views.cadastro_fornecedor, name="cadastro_fornecedor"),
     path('login/', views.login, name='login'),
 ]
