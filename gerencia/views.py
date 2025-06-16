2c66c826e464a010862f4c6dbf4b39815f5d4c6b
graci-ferreira <fgracif@gmail.com>
2025-06-16 20:08:17 -0300

    Update views.py

--- gerencia/views.py
+++ gerencia/views.py
@@ -216,7 +216,7 @@ def deletar_funcionario(request, id):
 
 @login_required
 def lista_usuario(request):
-    usuarios = Usuario.objects.all()
+    usuarios = User.objects.all()
     return render(request, "lista_usuario.html", {"usuarios": usuarios})
 
 def cadastro_usuario(request):
